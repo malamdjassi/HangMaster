fdb962f8a7c062afe7532099a259ec7fc920d419
Daniel Silva <dani.silva259@gmail.com>
2017-01-03 15:46:54 +0000

    class jogo aleterada - com erros , se poderes dar dicas pff

--- src/main/jogo.java
+++ src/main/jogo.java
@@ -3,19 +3,24 @@ package main;
 import java.io.IOException;
 
 public class jogo {
+	 dicionario d = new dicionario();
+	 enforcado p = new enforcado();
+	 static char letra;
+
 
 	public static void main(String[] args) throws IOException 
 	{
-	  enforcado p = new enforcado();
-	  dicionario d = new dicionario();
-	  char letra;
-	  int cont = 0;;
+		 
+		
+		 
+		
+		 
 	  do{
 	  System.out.println("Qual é a letra que escolhe?: ");
 	  letra = (char)System.in.read();
 	  
-	  if ()
-	  }while(cont == 6);
+	  //if ()
+	  }while(p.state == 6);
 	  
 	
 
@@ -25,11 +30,33 @@ public class jogo {
 	
 	public void updateGraph()
 	{
-		//actualiza a imagem do enforcado face ao enforcado.state (0-6) e a palavra
+		switch (p.state)
+		{
+		case 1: 
+			System.out.println("_____");
+			break;
+		case 2:
+			System.out.println("  |  \n  |  \n __|_");
+			break;
+		case 3:
+			System.out.println("  |  \n  |  \n  |  \n  |  \n __|_");
+			break;
+		case 4:
+			System.out.println("  |--- \n  |  \n  |  \n  |  \n __|_");
+			break;
+		case 5:
+			System.out.println("  |--- \n  |  \n  |  | \n  |  \n __|_");
+			break;
+		case 6:
+			System.out.println("  |--- \n  |  \n  |  | \n  |  0 \n __|_");
+			System.out.println("  PERDEU");
+			break;
+		}
+		
 	}
 
 	public void input(char letter)
 	{
-		letra
+		p.receiveLetter(this);
 	}
 }
