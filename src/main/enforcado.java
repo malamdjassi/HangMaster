3519a0d3a5c799a0d45fda10ddc155ba4b793468
IronMagician(Home) <ironmagician@yahoo.com>
2017-01-03 12:03:38 +0000

    definição das acções do enforcado

--- src/main/enforcado.java
+++ src/main/enforcado.java
@@ -13,11 +13,37 @@ public class enforcado
 	 * 5-braço esquerdo
 	 * 6-braço direito <--GAME OVER!
 	 */
-	
+	//COMENTÁRIO DO COELHO
 	public enforcado()
 	{
 		//construtor
 	}
 	
+	public String returnCrypt () //retorna palavra encriptada -> 'palavra' rescrita com letras de 'letrasRespondidas'
+	{
+		
+		return null;  //ex.: L _ I _ A O (leitao) com 'letrasRespondidas'->"LIAOP"
+	}
 	
+	public boolean isVictor() //recebe returnCrypt, se não tiver underscore, ganhou!
+	{
+		return false; //true se não existirem underscores em returnCrypt
+	}
+	
+	public void receiveLetter(char letter) //recebe letra e insere em 'letrasRespondidas'
+	{
+		//se a letra não existir em 'letrasRespondidas' nem em 'palavra'
+			//Incrementar 'state'
+		//se a letra  existir em 'letrasRespondidas' mas não em 'palavra'
+			//NADA
+		//se a letra não existir em 'letrasRespondidas' mas existir em 'palavra'
+			//Adiciona a 'letrasRespondidas'
+		//se a letra existir em 'letrasRespondidas' e em 'palavra' 
+			//NADA
+	}
+	
+	private boolean incrementer()
+	{
+		return false;
+	}
 }
